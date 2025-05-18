484b240b938f886d3a5e72ce326c005da33c4617
xfwdrev <frignacio16@gmail.com>
2025-05-19 02:47:52 +0800

    treewide: fix commit errors again

--- mm/madvise.c
+++ mm/madvise.c
@@ -167,14 +167,9 @@ success:
 	/*
 	 * vm_flags is protected by the mmap_sem held in write mode.
 	 */
-<<<<<<< HEAD
 	vm_write_begin(vma);
-	WRITE_ONCE(vma->vm_flags, new_flags);
+	WRITE_ONCE(vma->vm_flags, vma_pad_fixup_flags(vma, new_flags));
 	vm_write_end(vma);
-=======
-	vma->vm_flags = vma_pad_fixup_flags(vma, new_flags);
-
->>>>>>> 94424b0fcef48220df39e96622270e580ced9198
 out_convert_errno:
 	/*
 	 * madvise() returns EAGAIN if kernel resources, such as
