4a8aa64e1a707d51349cdc86d0e85142937d7e1a
xfwdrev <frignacio16@gmail.com>
2025-05-19 00:27:42 +0800

    fix commit errors

--- drivers/usb/storage/scsiglue.c
+++ drivers/usb/storage/scsiglue.c
@@ -447,8 +447,6 @@ static DEF_SCSI_QCMD(queuecommand)
 /* Command timeout and abort */
 static int command_abort_matching(struct us_data *us, struct scsi_cmnd *srb_match)
 {
-	struct us_data *us = host_to_us(srb->device->host);
-
 	usb_stor_dbg(us, "%s called\n", __func__);
 #ifdef CONFIG_USB_DEBUG_DETAILED_LOG
 	printk(KERN_ERR "usb-storage: %s scsi_lock +\n", __func__);
@@ -466,14 +464,14 @@ static int command_abort_matching(struct us_data *us, struct scsi_cmnd *srb_matc
 #ifdef CONFIG_USB_DEBUG_DETAILED_LOG
 		printk(KERN_ERR "usb-storage: %s -- nothing to abort -\n",
 				__func__);
-#endif		
+#endif	
 		return SUCCESS;
 	}
 
 	/* Does the command match the passed srb if any ? */
 	if (srb_match && us->srb != srb_match) {
 		scsi_unlock(us_to_host(us));
-		usb_stor_dbg(us, "-- pending command mismatch\n");
+		usb_stor_dbg(us, "-- pending command mismatch\n");	
 		return FAILED;
 	}
 
