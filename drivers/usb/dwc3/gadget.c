8c73f5993ac618d4234c91dca038c84f6f23cf61
xfwdrev <frignacio16@gmail.com>
2025-05-19 03:04:31 +0800

    drivers/usb/dwc3: fix commit error, again

--- drivers/usb/dwc3/gadget.c
+++ drivers/usb/dwc3/gadget.c
@@ -2147,22 +2147,11 @@ static int dwc3_gadget_run_stop(struct dwc3 *dwc, int is_on, int suspend)
 		reg &= DWC3_DSTS_DEVCTRLHLT;
 	} while (--timeout && !(!is_on ^ !reg));
 
-<<<<<<< HEAD
 	if (!timeout) {
 		dev_err(dwc->dev, "gadget %s timeout, DSTS : 0x%x\n",
 					is_on ? "run" : "stop", reg);
 		return 0;
 	}
-=======
-	if (saved_config) {
-		reg = dwc3_readl(dwc->regs, DWC3_GUSB2PHYCFG(0));
-		reg |= saved_config;
-		dwc3_writel(dwc->regs, DWC3_GUSB2PHYCFG(0), reg);
-	}
-
-	if (!timeout)
-		return -ETIMEDOUT;
->>>>>>> d6a7c6fab0fddf277c3e22ef83657ba74a2f3800
 
 	return 0;
 }
